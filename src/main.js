22602b1114a55b8631f239da61c34e1edc4b15ef
Gideon Peters <gideonpeters85@gmail.com>
2019-02-19 09:06:10 +0100

    new store spa

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
